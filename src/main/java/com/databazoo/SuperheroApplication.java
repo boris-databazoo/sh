12bb826389bb18bf1d87559273005fc453d0f707
Boris Schegolev <boris@databazoo.com>
2018-09-18 21:59:29 +0200

    more fixes for Heroku

--- src/main/java/com/databazoo/SuperheroApplication.java
+++ src/main/java/com/databazoo/SuperheroApplication.java
@@ -1,22 +1,16 @@
 package com.databazoo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:/app.properties")
-public class SuperheroApplication extends SpringBootServletInitializer {
+public class SuperheroApplication {
 
-    /**
-     * This override is required to run the app with a tomcat7 maven plugin.
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SuperheroApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheroApplication.class, args);
     }
-
 }
 
 
